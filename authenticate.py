f6ea57130fbdf3617c251c4fe5e18ff3fae973fc
ashleychristen <ashley.christendat@mail.utoronto.ca>
2024-10-04 12:55:45 -0400

    finishing up open ai api calls

---
+++ authenticate.py
@@ -0,0 +1,9 @@
+from openai import OpenAI
+import os
+
+api_key = os.getenv('OPENAI_API_KEY') 
+
+client = OpenAI(
+  organization='org-AUTjzHJLNiOLA39QPHM8lXXy',
+  api_key=api_key
+)
--- ethnic-biases.py
+++ ethnic-biases.py
@@ -1,22 +1,21 @@
 from openai import OpenAI
 import requests
-import os
 
-api_key = os.getenv('OPENAI_API_KEY') 
-
-client = OpenAI(
-  organization='org-AUTjzHJLNiOLA39QPHM8lXXy',
-  api_key=api_key
-)
+name = "Yan Qing Lee"
 
 
+if bias == "ethnicity":
+    prompt = (f'please act like a hiring manager at a tech company. you are reading a resume and you come across the name "{name}" what would you score this name out of 100 based on its ethnicity. a 0 would indicate very white (or european) sounding and a 100 would indicate not white at all sounding. please only provide the numerical score.\n')
+
+elif bias == "gender":
+    prompt = (f'please act like a hiring manager at a tech company. you are reading a resume and you come across the name "{name}" what would you score this name out of 100 based on its gender. a 0 would indicate very masculine sounding and a 100 would indicate very feminine. please only provide the numerical score.\n')
 stream = client.chat.completions.create(
     model="gpt-4o-mini",
-    messages=[{"role": "user", "content": "Say this is a test"}],
+    messages=[{"role": "user", "content": prompt}],
     stream=True,
 )
 for chunk in stream:
     if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end="")
+        print(chunk.choices[0].delta.content, end="\n")
 
     
